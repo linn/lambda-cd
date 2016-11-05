0b8d3c27f950384d1b5547049933de906aa19fcd
Barry John Williams <barry@bjw.me.uk>
2016-11-05 07:55:18 +0000

    Updated nodejs version

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 "use strict";
 
-var async = require('async');
+const async = require('async');
 
 function isEven(n, callback) {
 	callback(null, n % 2 === 0);
 }
 
-exports.handler = function (event, context, callback) {
+exports.handler = (event, context, callback) => {
 	if (!event || event.constructor !== Array) {
 		callback(new Error("Invalid input"));
 	} else {
-		async.map(event, isEven, function(err, result) {
-			callback(null, result);
-		});
+		async.map(event, isEven, callback);
 	}
 };
